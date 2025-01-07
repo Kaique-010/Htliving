6df9935b77ab55640eaf804bfbdebf8023306a58
Leo <leokaique7@gmail.com>
2025-01-07 11:10:50 -0300

    Settings

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,4 +1,6 @@
 # backend/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path, include
@@ -8,3 +10,5 @@ urlpatterns = [
     path('api/', include('gemini_api.urls')),  # Incluir as rotas da app gemini_api
     path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
