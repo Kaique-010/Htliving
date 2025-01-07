e21cf609b1013add7af8ca51f2461f22001cdc77
Leo <leokaique7@gmail.com>
2025-01-07 10:58:09 -0300

    Settings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',  # Certifique-se de que isso esteja aqui
     'rest_framework',
     'gemini_api', 
     'corsheaders',
--- backend/app/wsgi.py
+++ backend/app/wsgi.py
@@ -1,9 +1,7 @@
-
-
 import os
+import sys
 
+sys.path.insert(0, '/home/Leokaique10/Htliving/backend')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')  # Ajuste aqui
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
 application = get_wsgi_application()
