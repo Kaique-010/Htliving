b905b79992f163a5ffc6e9d92fb0a62fdd306b36
Leo <leokaique7@gmail.com>
2025-01-07 11:20:11 -0300

    Settings

--- backend/gemini_api/urls.py
+++ backend/gemini_api/urls.py
@@ -1,4 +1,9 @@
 # gemini_api/urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DietaViewSet, GeminiPromptApiView
@@ -16,4 +21,9 @@ urlpatterns = [
 
     # URL para a view GeminiPromptApiView
     path('gemini-prompt/', GeminiPromptApiView.as_view(), name='gemini-prompt'),
+    
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
